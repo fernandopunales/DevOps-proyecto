9f95b89d46a89226057d39ef5c7a4e6bd7e49c48
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-22 23:13:09 -0300

    Update variables.tf

--- infra/terraform/modules/security-group/variables.tf
+++ infra/terraform/modules/security-group/variables.tf
@@ -2,4 +2,3 @@ variable "vpc_id" { type = string }
 variable "vpc_cidr" { type = string }
 variable "environment" { type = string }
 variable "api_gateway_port" { type = number }
-variable "alb_sg_id" { type = string }
